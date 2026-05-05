cd7521fb0f8e0e10e7d57bfd2aa168b57cca2539
XaverNajafova <nurixaver@gmail.com>
2026-05-05 14:41:11 +0400

    en yeni

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -19,26 +21,45 @@ public class BaseTest {
         String browser = System.getProperty("browser", "chrome");
 
         if (browser.equalsIgnoreCase("chrome")) {
+
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        }
-        else if (browser.equalsIgnoreCase("edge")) {
-            WebDriverManager.edgedriver().setup();
-            driver = new EdgeDriver();
-        }
-        else {
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            options.addArguments("--remote-allow-origins=*");
+
+            driver = new ChromeDriver(options);
+
+        } else if (browser.equalsIgnoreCase("edge")) {
+
+            try {
+                WebDriverManager.edgedriver()
+                        .clearResolutionCache()
+                        .setup();
+
+                EdgeOptions options = new EdgeOptions();
+                options.addArguments("--start-maximized");
+                options.addArguments("--remote-allow-origins=*");
+
+                driver = new EdgeDriver(options);
+
+            } catch (Exception e) {
+                throw new RuntimeException(
+                        "EdgeDriver failed. Check internet/DNS or WebDriverManager config: " + e.getMessage()
+                );
+            }
+
+        } else {
             throw new RuntimeException("Browser not supported: " + browser);
         }
 
         System.out.println("Running on browser: " + browser);
 
-        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterMethod
     public void tearDown() {
-
         if (driver != null) {
             driver.quit();
         }
